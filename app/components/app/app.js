8a14d76f41ed21dc0708e9e05c146d9cf9dd8331
Igor Yakutovich <igor@yakutovi.ch>
2016-06-06 18:20:50 +0300

    removed separate recipes component in favor of app component

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 
-import {Recipes} from 'components';
+import {intersect, diff, withoutIdx} from 'utils/array';
+import {parseInitialRecipeStrings, compareRecipes} from 'utils/recipe';
+import {AddPartForm, AvailableRecipesList, AvailablePartBadge} from 'components';
 import initialRecipeList from 'recipeLists/initial';
 
-export default props => {
-  return (
-    <div className="container">
-      <Recipes initialRecipeList={initialRecipeList}/>
-    </div>
-  )
+export default class App extends Component {
+  state = {
+    availableParts: [],
+    recipes: parseInitialRecipeStrings(initialRecipeList)
+  };
+
+  calculateAvailableRecipes() {
+    const {availableParts} = this.state;
+
+    return this.state.recipes
+      .map(recipe => Object.assign(recipe, {
+        partsHave: intersect(recipe.parts, availableParts),
+        partsLeft: diff(recipe.parts, availableParts)
+      }))
+      .sort(compareRecipes);
+  }
+
+  onAddAvailablePart(part) {
+    this.setState({availableParts: this.state.availableParts.slice(0).concat([part])});
+  }
+
+  removeAvailablePart(index) {
+    this.setState({availableParts: withoutIdx(this.state.availableParts, index)});
+  }
+
+  renderAvailablePartBadge(part, idx) {
+    return <AvailablePartBadge key={idx} part={part} onRemove={() => this.removeAvailablePart(idx)}/>;
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <div className="card-block">
+          <AddPartForm onAdd={::this.onAddAvailablePart} />
+
+          <div>
+            <h4>You're now having the following parts:</h4>
+            <div>
+              {this.state.availableParts.map(::this.renderAvailablePartBadge)}
+            </div>
+          </div>
+
+          <div>
+            <h4>Available recipes:</h4>
+            <AvailableRecipesList availableRecipes={::this.calculateAvailableRecipes()}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
--- app/components/index.js
+++ app/components/index.js
@@ -1,5 +1,4 @@
 export App from './app/app';
-export Recipes from './recipes/recipes';
 export AddPartForm from './addPartForm/addPartForm';
 export AvailableRecipesList from './availableRecipesList/availableRecipesList';
 export AvailablePartBadge from './availablePartBadge/availablePartBadge';
